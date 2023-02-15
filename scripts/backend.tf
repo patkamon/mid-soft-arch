d7e097e6c40dc65eb975cc7cecb9bbf0702cbdb7
patkamon <patkamon.a@ku.th>
2023-02-15 15:46:31 +0700

    change resource group

--- scripts/backend.tf
+++ scripts/backend.tf
@@ -1,9 +1,9 @@
 terraform {
     # Uncomment this to get it running in the CD pipeline.
      backend "azurerm" {
-         resource_group_name  = "new_uniq_tube"
-         storage_account_name = "flixtubeuniqq"
-         container_name       = "flixtubecontainer"
+         resource_group_name  = "new_one"
+         storage_account_name = "newflixtube"
+         container_name       = "terraform"
          key                  = "terraform.tfstate"
      }
 }
